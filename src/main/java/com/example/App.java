0f3ae5e09c991abf84414e0bee420e8679b4d81e
gora <gorachandpradhan75@gmail.com>
2026-07-28 00:58:27 +0530

    Add SimpleAdder Maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = add(num1, num2);
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Sum = " + sum);
+    }
+}
